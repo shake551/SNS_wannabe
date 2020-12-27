4c705289d1bd6ac258483db4bdcfc3e5be88147a
shake <hiroki.yamako@ezweb.ne.jp>
2020-12-27 22:54:45 +0900

    3-4

--- hello/views.py
+++ hello/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
-from .forms import HelloForm
+from .forms import FriendForm
 from .models import Friend
+from django.db.models import QuerySet
 
 def index(request):
+    data = Friend.objects.all()
     params = {
         'title': 'Hello',
-        'message': 'all friends. ',
-        'form': HelloForm(),
-        'data': [],
+        'data': data,
     }
-    if (request.method == 'POST'):
-        num = request.POST['id']
-        item = Friend.objects.get(id=num)
-        params['data'] = [item]
-        params['form'] = HelloForm(request.POST)
-    else:
-        params['data'] = Friend.objects.all()
     return render(request, 'hello/index.html', params)
+
+#create model
+def create(request):
+    if (request.method == 'POST'):
+        obj = Friend()
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'form': FriendForm(),
+    }
+    return render(request, 'hello/create.html', params)
+
+def edit(request, num):
+    obj = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend  =FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'id': num,
+        'form': FriendForm(instance=obj),
+    }
+    return render(request, 'hello/edit.html', params)
