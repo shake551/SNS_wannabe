96a5609e8d136eca4554d82de663e6e5bdde0e84
shake <hiroki.yamako@ezweb.ne.jp>
2020-12-27 22:57:33 +0900

    3-4

--- hello/forms.py
+++ hello/forms.py
@@ -1,4 +1,19 @@
 from django import forms
+from.models import Friend
 
 class HelloForm(forms.Form):
-    id = forms.IntegerField(label='ID')
+    name = forms.CharField(label='Name', \
+        widget=forms.TextInput(attrs={'class':'form-control'}))
+    mail = forms.EmailField(label='Email', \
+        widget=forms.EmailInput(attrs={'class':'form-control'}))
+    gender = forms.BooleanField(label='Gender', required=False, \
+        widget=forms.CheckboxInput(attrs={'class':'form-check'}))
+    age = forms.IntegerField(label='Age', \
+        widget=forms.NumberInput(attrs={'class':'form-control'}))
+    birthday = forms.DateField(label='Birth', \
+        widget=forms.DateInput(attrs={'class':'form-control'}))
+
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name', 'mail', 'gender', 'age', 'birthday']
