fdd8b0d110cc4e6e92807cc3b8ce0204b182c6eb
shake <hiroki.yamako@ezweb.ne.jp>
2020-12-29 11:48:59 +0900

    3-5

--- hello/urls.py
+++ hello/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+
 from . import views
+from .views import FriendList
+from .views import FriendDetail
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('create', views.create, name='create'),
     path('edit/<int:num>', views.edit, name='edit'),
+    path('delete/<int:num>', views.delete, name='delete'),
+    path('list', FriendList.as_view()),
+    path('detail/<int:pk>', FriendDetail.as_view()),
+    path('find', views.find, name='find'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
+from django.db.models import QuerySet
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.db.models import Q
+
 from .forms import FriendForm
 from .models import Friend
-from django.db.models import QuerySet
+from .forms import FindForm
 
 def index(request):
-    data = Friend.objects.all()
+    data = Friend.objects.all().order_by('age').reverse()
     params = {
         'title': 'Hello',
+        'message': '',
         'data': data,
     }
     return render(request, 'hello/index.html', params)
@@ -39,3 +45,40 @@ def edit(request, num):
         'form': FriendForm(instance=obj),
     }
     return render(request, 'hello/edit.html', params)
+
+def delete(request, num):
+    friend = Friend.objects.get(id=num)
+    if (request.method == 'POST'):
+        friend.delete()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'id': num,
+        'obj': friend,
+    }
+    return render(request, 'hello/delete.html', params)
+
+class FriendList(ListView):
+    model = Friend
+
+class FriendDetail(DetailView):
+    model = Friend
+
+def find(request):
+    if (request.method == 'POST'):
+        msg = 'search result:'
+        form = FindForm(request.POST)
+        find = request.POST['find']
+        list = find.split()
+        data = Friend.objects.filter(name__in=list)
+    else:
+        msg = 'search words...'
+        form = FindForm()
+        data = Friend.objects.all()
+    params = {
+        'title': 'Hello',
+        'message': msg,
+        'form': form,
+        'data': data,
+    }
+    return render(request, 'hello/find.html', params)
